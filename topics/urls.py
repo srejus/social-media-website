78a04d9f2ec1ca89b13adfbdd811eba400092433
srejus <srejusjayaakumar@gmail.com>
2021-11-14 10:45:18 +0530

    some changes

--- topics/urls.py
+++ topics/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('follow_topic/<int:id>',views.follow_topic,name='follow_topic'),
+    path('unfollow_topic/<int:id>',views.unfollow_topic,name='unfollow_topic'),
     path('topic/<int:id>',views.topic,name='topic'),
 ]
--- topics/views.py
+++ topics/views.py
@@ -6,14 +6,38 @@ from topics.models import Topic, Topic_follow
 # Create your views here.
 def follow_topic(request,id):
     usr = request.user
-    topic = Topic_follow.objects.get(topic_id = id)
-    if usr in topic.user_id:
-        print("Exists....")
-    else:
-        print("Does not exists...")
-    
+    tpc =Topic.objects.get(id = id)
+    try:
+        x=Topic_follow.objects(user_id = request.user , topic_follow = tpc)
+        x.save()
+    except:
+        pass
     return HttpResponse("Ok")
 
+def unfollow_topic(request,id):
+    usr = request.user
+    tpc =Topic.objects.get(id = id)
+    if Topic_follow.objects.filter(user_id = request.user , topic_id = tpc).exists():
+        x=Topic_follow.objects.get(user_id = request.user , topic_id = tpc)
+        x.delete()
+
+    return HttpResponse("Ok")
+ 
+ #Code works when click on view button
 def topic(request,id):
     tpc =Topic.objects.get(id = id)
-    return render(request,'topic_info.html')
+
+    flw = False
+
+    #Count total number of followers for this particular topic
+
+    count = Topic_follow.objects.filter(topic_id = tpc).count()
+
+
+    
+    #Check whether the user follow this topic or not
+    if Topic_follow.objects.filter(user_id = request.user , topic_id = tpc).exists():
+        flw = True
+    else:
+        flw = False
+    return render(request,'topic_info.html',{'tp':tpc,'fw':flw,'c':count})
